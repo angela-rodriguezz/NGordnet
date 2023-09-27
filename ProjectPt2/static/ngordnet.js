19c4edd5a3488df7594ed67e6594135e9e1dee70
Angela <amr25@berkeley.edu>
2023-09-27 02:24:29 -0500

    maybe?

--- ProjectPt2/static/ngordnet.js
+++ ProjectPt2/static/ngordnet.js
@@ -4,7 +4,7 @@ $(function() {
 
 	var host;
 
-    host = 'http://localhost:4567';
+    host = 'http://www.angelarodriguezz.me/NGordnet';
     const history_server = host + '/history';
     const historytext_server = host + '/historytext';
     const hyponyms_server = host + '/hyponyms';
